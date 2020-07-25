f9b817790ee6b9c9a380c0d4f35e6cf79c26259a
Flexlug <flexlug@outlook.com>
2020-07-25 11:48:11 +0300

    Created project Library and console executable. Added dependences to create requests and handle JSON responses (RestSharp and Newtonsoft.JSON).

---
+++ BulkDownloader-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BulkDownloader_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BulkDownloader/Downloader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace BulkDownloader
+{
+    public class Downloader
+    {
+        private readonly string _token;
+
+        public Downloader(string email, string password)
+        {
+
+        }
+    }
+}
