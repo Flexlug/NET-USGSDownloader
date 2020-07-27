e77f66008d66bc05ecdd9e3e813def80bf67a03d
Flexlug <flexlug@outlook.com>
2020-07-27 11:36:04 +0300

    Added comments to DatasetRequest properties.

--- BulkDownloader/RequestTemplates/DatasetRequest.cs
+++ BulkDownloader/RequestTemplates/DatasetRequest.cs
@@ -11,9 +11,15 @@ namespace BulkDownloader.RequestTemplates
     /// </summary>
     public class DatasetRequest
     {
+        /// <summary>
+        /// The dataset identifier - must use this or datasetName
+        /// </summary>
         [JsonProperty("datasetId")]
         public string DatasetId { get; set; }
 
+        /// <summary>
+        /// The system-friendly dataset name - must use this or datasetId
+        /// </summary>
         [JsonProperty("datasetName")]
         public string DatasetName { get; set; }
     }
